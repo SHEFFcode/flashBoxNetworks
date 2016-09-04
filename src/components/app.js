4318b856b35aecf057966cda95f7056dbedd306d
SHEFFcode <sheff@sheffmachine.com>
2016-09-04 13:16:27 -0700

    Basic flashbox website layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
-import Intro from './Intro'
+import Intro from './Intro';
+import Contact from './Contact';
+import Footer from './Footer';
 
 export default class App extends Component {
   render() {
@@ -8,6 +10,8 @@ export default class App extends Component {
       <div>
         <Navbar></Navbar>
         <Intro></Intro>
+        <Contact></Contact>
+        <Footer></Footer>
       </div>
     );
   }
